ab00afc7fe564dfe7902fdcd5915aeec8612d499
ankitak2898 <69913033+ankitak2898@users.noreply.github.com>
2020-09-01 21:15:05 +0530

    Add files via upload

--- day2q4.js
+++ day2q4.js
@@ -1,8 +1,10 @@
 let friends = ["Rachel","Ross","Monica","Chandler","Joey","Pheobe"];
+
+
 for(let i=0;i<=5;i++)
 {
-if (friends.indexOf('a')>=0)
-{
-console.log(friends(i));
-}
+    let index = friends[i].indexOf("a");
+    
+    if (index >= 1 && index <= 5)
+console.log(friends[i]);
 }
